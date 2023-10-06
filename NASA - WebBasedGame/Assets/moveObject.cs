77f4dccca4a3371e5a32ce5577c7387599acd9c6
Speedister <krskabel@asu.edu>
2023-10-06 12:04:10 -0700

    Testing Click Event

--- NASA - WebBasedGame/Assets/moveObject.cs
+++ NASA - WebBasedGame/Assets/moveObject.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class moveObject : MonoBehaviour
 {
+    Rigidbody2D rigid;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     private void OnMouseDown()
     {
-        GameObject theObject = GetComponent<GameObject>();
+        //GameObject theObject = GetComponent<GameObject>();
 
-        
+        //MoveDirection direction = MoveDirection.Left;
+
+        rigid.MovePosition(Input.mousePosition);
     }
 
     // Update is called once per frame
