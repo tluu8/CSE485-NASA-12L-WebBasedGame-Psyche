b387eb0a80d6aa033ad6355d69f709b0da3d92fc
Speedister <krskabel@asu.edu>
2023-10-06 13:25:31 -0700

    Update Inventory.cs

--- NASA - WebBasedGame/Assets/Script/Inventory.cs
+++ NASA - WebBasedGame/Assets/Script/Inventory.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public class Item
+    {
+        string name;
+    }
+    public class Slot
+    {
+        Item item;
+        int slotNum;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
