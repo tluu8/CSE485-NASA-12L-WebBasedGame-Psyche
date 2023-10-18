92b3e3e02b3dd1cec1ee24729b19e39b7a135728
tluu8 <tluu8@asu.edu>
2023-10-17 23:33:31 -0700

    Develop Credit scene
    Design scene and make transition back to the title scene by creating Back button

--- NASA - WebBasedGame/Assets/Script/Menu.cs
+++ NASA - WebBasedGame/Assets/Script/Menu.cs
@@ -7,8 +7,22 @@ public class Menu : MonoBehaviour
 {   
 
     // Start game function
-    public void StartGame() {
+    public void StartGameButton() {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    // Credit button function
+    public void CreditButton() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    // Bakc button function
+    public void BackButton()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
 }
