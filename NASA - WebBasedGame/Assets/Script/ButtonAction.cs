21f173c90a0c1a41283c3ff7caf629ef33374e0f
tluu8 <tluu8@asu.edu>
2023-10-19 21:04:17 -0700

    Transition from intro to game scene
    Using timeline for scene loader object to create the transition from intro scene to the main game scene

--- NASA - WebBasedGame/Assets/Script/ButtonAction.cs
+++ NASA - WebBasedGame/Assets/Script/ButtonAction.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class ButtonAction : MonoBehaviour
 {   
 
-    // Start game function
+    // Click this button to move to the first game scene
     public void StartGameButton() {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Credit button function
+    // Click this button to move to the credit scene
     public void CreditButton() {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
-    // Back button function
-    public void BackButton()
-    {
+    // Click this button to move back to the menu scene
+    public void BackButton() { 
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
-
+    
 }
---
+++ NASA - WebBasedGame/Assets/Script/SceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Load starting game scene
+    public void OnEnable()
+    {
+
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+}
