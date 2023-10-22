38bedbf83fa3f8a33885a4d6310399306235b706
psshivan <psshivan@asu.edu>
2023-10-22 11:57:02 -0700

    Test Object added to GameScene

---
+++ NASA - WebBasedGame/Assets/Script/FixedYPosition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedYPosition : MonoBehaviour
+{
+    public float fixedY = -3.495f; // The Y coordinate you want to fix the GameObject at
+
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+
+        // Check if the current Y position is different from the fixed Y position
+        if (currentPosition.y != fixedY)
+        {
+            // If so, update the Y position while keeping X and Z the same
+            transform.position = new Vector3(currentPosition.x, fixedY, currentPosition.z);
+        }
+    }
+}
