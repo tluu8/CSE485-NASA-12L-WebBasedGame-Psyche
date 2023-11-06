b7daeb4b4e94b4e3a72e2f3c88e3efcffd02a1d1
Speedister <krskabel@asu.edu>
2023-11-05 20:02:18 -0700

    Updates to the InventoryUI

--- NASA - WebBasedGame/Assets/Script/InventorySystem.cs
+++ NASA - WebBasedGame/Assets/Script/InventorySystem.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using JetBrains.Annotations;
+using System.IO;
+using Unity.VisualScripting;
 
 public class InventorySystem : MonoBehaviour
 {
     public List<GameObject> items = new List<GameObject>();
+    public List<string> descriptions = new List<string>();
+    public List<Texture2D> displayIcons = new List<Texture2D>();
+    private int itemNum = 1;
 
     public void AddItem(GameObject item)
     {
         items.Add(item);
+        descriptions.Add("Test Description #" + itemNum);
+        Texture2D iconToAdd = new Texture2D(16, 16);
+        iconToAdd = Texture2D.blackTexture;
+        displayIcons.Add(iconToAdd);
+        itemNum++;
     }
 }
