c656776817f88818e53855639f6d12a1053f9021
tluu8 <tluu8@asu.edu>
2023-11-13 12:55:20 -0700

    Add continue button
    Continue button is functional and moves the intro to the game scene when it is clicked.

--- NASA - WebBasedGame/Assets/Script/ButtonAction.cs
+++ NASA - WebBasedGame/Assets/Script/ButtonAction.cs
@@ -6,22 +6,28 @@ using UnityEngine.SceneManagement;
 public class ButtonAction : MonoBehaviour
 {   
 
-    // Click this button to move to the first game scene
+    // Click this button (menu scene) to move to the first game scene
     public void StartGameButton() {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Click this button to move to the credit scene
+    // Click this button (menu scene) to move to the credit scene
     public void CreditButton() {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
-    // Click this button to move back to the menu scene
+    // Click this button (credit scene) to move back to the menu scene
     public void BackButton() { 
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
-    
+
+    // Click this button (intro scene) to move to the game scene
+    public void ContinueButton()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
 }
