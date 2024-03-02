880e6b0aed1e60799ad9509a2673b8c872908fa9
tluu8 <tluu8@asu.edu>
2024-03-02 11:00:23 -0700

    Add volume setting
    Created a bar to adjust the music volume in Pause menu

--- NASA - WebBasedGame/Assets/Script/PlaySoundEffect.cs
+++ NASA - WebBasedGame/Assets/Script/PlaySoundEffect.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaySoundEffect : MonoBehaviour { 
 
     public AudioSource audio;   // Create AudioSource object
+    public Slider volumeSlider;     // Create Slider object
 
     // Play audio file method
     public void playSound() { 
 
         audio.Play();
+    }
+
+    // Set volume at 100% at the begining method
+    void Start() {
+
+        volumeSlider.value = 1;
+    }
+
+    // Update the volume while sliding the volume bar method
+    public void changeVolume() {
+
+        AudioListener.volume = volumeSlider.value;
     }
 }
