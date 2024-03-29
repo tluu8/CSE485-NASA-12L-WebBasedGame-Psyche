5246ae6287560788cf3a83df7e302001a29e6431
Speedister <krskabel@asu.edu>
2024-03-29 15:33:17 -0700

    Cleaning up old code

--- NASA - WebBasedGame/Assets/Script/InventorySystem.cs
+++ NASA - WebBasedGame/Assets/Script/InventorySystem.cs
@@ -11,8 +11,6 @@ public class InventorySystem : MonoBehaviour
     public List<GameObject> items = new List<GameObject>();
     public Dictionary<string, string> descriptions = new Dictionary<string, string>();
     public Dictionary<string, string> combinableItems = new Dictionary<string, string>();
-    //public Dictionary<string, string> imageSprites = new Dictionary<string, string>();
-    //private int itemNum = 1;
 
     private void Start()
     {
@@ -84,7 +82,5 @@ public class InventorySystem : MonoBehaviour
     public void AddItem(GameObject item)
     {
         items.Add(item);
-        //descriptions.Add(item.name, "Test Description #" + itemNum);
-        //itemNum++;
     }
 }
